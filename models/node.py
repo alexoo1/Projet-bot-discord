4f7f2e5d8f27f26e0af3e523929983f8c51fad7f
arthur-fontaine <0arthur.fontaine@gmail.com>
2022-05-31 15:28:21 +0200

    feat: add a node class

---
+++ models/node.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from typing import Callable, List, Optional
+
+
+class Node:  # create a class Node
+    def __init__(self, callback: Optional[Callable] = None, name: Optional[str] = None):  # initialize the Node with a callback and a name
+        self.children: List[Node] = []
+        self.callback: Optional[Callable] = callback
+        self.name: Optional[str] = name
+
+    def run(self, *args, **kwargs):
+        if self.callback:  # check that the callback is defined
+            self.callback(*args, **kwargs)  # run the callback
+
+    def add_child(self, child: Node):
+        self.children.append(child)  # add a child to the Node (like a tree)
+
+    def __call__(self, *args, **kwargs):
+        self.run(*args, **kwargs)  # make possible to use a Node instance as a function (node = Node(); node())
+
+    def __getattr__(self, item):
+        return next((child for child in self.children if child.name == item), None)  # return the first child found by name
+
+    def __getitem__(self, item):  # make possible to use a Node instance as a list or a dictionary (node = Node(); node[0]; node['name'])
+        if isinstance(item, int):  # if the item is an integer
+            return self.children[item]  # return the child at the index (like a list)
+        elif isinstance(item, str):  # if the item is a string
+            return self.__getattr__(item)  # return the child by name (like a dictionary) (see __getattr__)
+        else:
+            raise TypeError(f"{type(item)} is not supported")  # raise an error if the item is not an integer or a string
