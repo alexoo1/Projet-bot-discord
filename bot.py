e36fcecc14d416875e418896b181d59c07495b12
alexoo1 <99253586+alexoo1@users.noreply.github.com>
2022-05-31 09:47:48 +0200

    feat: add welcome message

--- bot.py
+++ bot.py
@@ -9,3 +9,8 @@ bot = commands.Bot(intents=intents)
 @bot.event
 async def on_ready():
     print("The bot is ready!")
+
+@bot.event
+async def on_member_join(member):
+    general_channel = bot.get_channel(980735769595510816)
+    await general_channel.send(f"Bienvenue sur le serveur {member.display_name} !")
