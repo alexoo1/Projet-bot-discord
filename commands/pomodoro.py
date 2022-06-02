b7deb25d873d1e719afcf37b1c6488ff73215e4e
arthur-fontaine <0arthur.fontaine@gmail.com>
2022-06-02 23:06:25 +0200

    fix: add some fixes

--- commands/pomodoro.py
+++ commands/pomodoro.py
@@ -1,26 +1,18 @@
 from bot import bot
 from commands.command import Command
 from handlers.pomodoro import PomodoroHandler
-import time
-import asyncio
+
+pomodoro_handler_instance = PomodoroHandler()
+
 
 class PomodoroCommand(Command):
     def __init__(self):
-        self.state = {'pomodoro': {}}
-        self.__interval()
-
-    async def __interval(self):
-        now = time.time()
-        for user_id, end_time in self.state['pomodoro'].items():
-            if end_time - now > 0:
-                bot.get_user(user_id).send('Time for a break boss')
-                del self.state['pomodoro'][user_id]
-        await asyncio.sleep(60 * 5)          
-        await self.__interval()
+        pass
 
     @bot.slash_command()
-    async def pomodoro():
-        await PomodoroHandler.pomodoro()
+    async def pomodoro(self, inter):
+        """Starts a pomodoro"""
+        await pomodoro_handler_instance.timer(inter)
 
     @staticmethod
     def setup():
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from bot import bot
 from dotenv import load_dotenv
 import os
 from commands.ping import PingCommand
+from commands.pomodoro import PomodoroCommand
 from handlers.hello import HelloHandler
 
 load_dotenv()
 
 PingCommand.setup()
 HelloHandler.setup()
+PomodoroCommand.setup()
 
 bot.run(os.getenv("DISCORD_BOT_TOKEN"))
